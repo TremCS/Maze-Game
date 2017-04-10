289d3701d7a938daf3b7f28b7245fbb1bd1f345b
TremCS <mattdcox@outlook.com>
2017-04-10 13:50:13 +0100

    Add files via upload

---
+++ Project/include/camera.h
@@ -0,0 +1,42 @@
+/*#ifndef CAMERA_H
+#define CAMERA_H
+#include <ngl/NGLInit.h>
+#include <ngl/Camera.h>
+#include <SDL.h>
+#include <ngl/Vec3.h>
+
+
+class camera {
+    private:
+
+          ngl::Vec3 m_rotation;
+
+//        ngl::Vec3 m_pos;
+//        ngl::Vec3 m_speed;
+
+//        double TO_RADS;
+
+//        bool m_forward;
+//        bool m_backward;
+//        bool m_strafeLeft;
+//        bool m_strafeRight;
+
+    public:
+        camera();
+        //~camera();
+
+        void lookAround(double mouseX, double mouseY);
+
+        void eventHandler(SDL_Event *_event);
+
+        void update(double m_rot_x, double m_rot_y);
+
+        void moveAround(int key, bool pressed);
+
+        double toRads(const double &degs) const;
+
+
+};
+
+#endif // CAMERA_H
+*/
